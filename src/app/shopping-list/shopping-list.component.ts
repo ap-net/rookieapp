e5b5f674a38b324f17ce1d93c451e5224a873ee2
AdrianoPicolo <adrian.piekarz1@wp.pl>
2023-02-04 22:27:48 +0100

    Added ingredients area

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredient.model';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Bananas', 5),
+    new Ingredient('Potatoes', 8),
+  ];
 
   constructor() { }
 
