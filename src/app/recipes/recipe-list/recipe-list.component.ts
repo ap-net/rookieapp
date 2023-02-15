00c46adcb97dad6d8810c941cd294767b4ada556
AdrianoPicolo <adrian.piekarz1@wp.pl>
2023-02-15 10:59:59 +0100

    recipe list changes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,13 +7,20 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('Test recipe', 'This is a test', 'https://www.theroastedroot.net/wp-content/uploads/2021/10/creamy-paleo-mushroom-chicken-7-720x1080.jpg.webp')
+    new Recipe('Test recipe', 'This is a test', 'https://www.theroastedroot.net/wp-content/uploads/2021/10/creamy-paleo-mushroom-chicken-7-720x1080.jpg.webp'),
+    new Recipe('Test recipe', 'This is another test', 'https://www.theroastedroot.net/wp-content/uploads/2021/10/creamy-paleo-mushroom-chicken-7-720x1080.jpg.webp')
   ];
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  onRecipeSelected (recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
+
 
 }
